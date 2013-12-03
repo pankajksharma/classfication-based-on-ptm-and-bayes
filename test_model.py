8dab1d849a4b081fef58355cc7068ff2e8e43103
Pankaj Kumar Sharma <sharmapankaj1992@gmail.com>
2013-12-03 15:43:41 +0530

    test edit

--- test_model.py
+++ test_model.py
@@ -1,5 +1,6 @@
 import json, MySQLdb
 from math import log
+from operator import itemgetter
 from lib.mysql import connection
 
 def update_occurences(cat, dp):
@@ -12,18 +13,19 @@ def update_occurences(cat, dp):
 
 MODEL_DIR = "model/"
 
-categories = ["TOPICS", "ORGS", "EXCHANGES", "COMPANIES", "PLACES", "PEOPLE"]
+categories = ["TOPICS", "ORGS", "EXCHANGES", "PLACES", "PEOPLE"]
 
 category_counts = {
 				"TOPICS" : json.load(open(MODEL_DIR+"topics.json")), 
 				"ORGS" : json.load(open(MODEL_DIR+"orgs.json")),
 				"EXCHANGES" : json.load(open(MODEL_DIR+"exchanges.json")), 
-				"COMPANIES" : json.load(open(MODEL_DIR+"companies.json")), 
 				"PLACES" : json.load(open(MODEL_DIR+"places.json")), 
 				"PEOPLE" : json.load(open(MODEL_DIR+"people.json"))
 			}
 
 counts = json.load(open(MODEL_DIR+"data.json"))
+
+sump = sum(c for c in counts.values())*1.0
 
 cursor = connection.cursor()
 query = "select cats, data from r21578 where type='test'"
@@ -34,7 +36,7 @@ while True:
 	if not c:
 		break
 	# print c[0]
-	probability = {"TOPICS":1, "ORGS":1, "EXCHANGES":1, "COMPANIES":1, "PLACES":1, "PEOPLE":1}
+	probability = {"TOPICS":1, "ORGS":1, "EXCHANGES":1, "PLACES":1, "PEOPLE":1}
 	cats = json.loads(c[0])
 	sents = json.loads(c[1])
 	for words in sents:
@@ -47,10 +49,17 @@ while True:
 			if foureir_applied:
 				for cat in categories:
 					try:
-						probability[cat] *= log((category_counts[cat][word]+1)/(counts[cat]+1))
+						probability[cat] *= -log((category_counts[cat][word]+1.0)/(counts[cat]+1.0))
 					except:
-						probability[cat] *= log(1.0/(counts[cat]+1))
+						probability[cat] *= -log(1.0/(counts[cat]+1.0))
 			else:
 				for cat in categories:
-					probability[cat] *= log(category_counts[cat][word]/counts[cat])
-	print probability,cats
+					probability[cat] *= -log(category_counts[cat][word]/(counts[cat]*1.0))
+
+	for cat in categories:
+		probability[cat] *= -log(counts[cat]/sump)
+
+	src = sorted(probability.keys(), key=itemgetter(1))
+	src.reverse()
+	print src
+	print cats
